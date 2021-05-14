a15ed2a38e9e1a61e01768213944d137c7006024
jrobertorma <jrobertorma@gmail.com>
2021-05-14 17:01:47 -0500

    Playing with functions and vars

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,5 +1,9 @@
 import 'package:dart_playground/dart_playground.dart' as dart_playground;
+import 'package:dart_playground/variables.dart' as variables;
 
 void main(List<String> arguments) {
+  var number = 6;
+
   print('Hello world: ${dart_playground.calculate()}!');
+  print('This is the square of $number : ${variables.square(number)}');
 }
---
+++ lib/variables.dart
@@ -0,0 +1,3 @@
+int square(int number) {
+  return (number * number);
+}
