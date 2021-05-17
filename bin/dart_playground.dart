5c557d2dbf024c6d004afa0643b1801c20554647
jrobertorma <jrobertorma@gmail.com>
2021-05-17 17:55:39 -0500

    Lists(arrays) basics

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,9 +1,15 @@
-import 'package:dart_playground/dart_playground.dart' as dart_playground;
-import 'package:dart_playground/variables.dart' as variables;
+// import 'package:dart_playground/dart_playground.dart' as dart_playground;
+// import 'package:dart_playground/variables.dart' as variables;
+
+import 'package:dart_playground/lists.dart' as lists;
 
 void main(List<String> arguments) {
-  var number = 6;
+  // var number = 6;
 
-  print('Hello world: ${dart_playground.calculate()}!');
-  print('This is the square of $number : ${variables.square(number)}');
+  // print('Hello world: ${dart_playground.calculate()}!');
+  // print('This is the square of $number : ${variables.square(number)}');
+  var exampleList = lists.basicList(); //[1,2,44,22], see lib\lists.dart
+
+  print(exampleList);
+  print(exampleList.indexOf(22));
 }
---
+++ lib/lists.dart
@@ -0,0 +1,27 @@
+import "dart:math";
+import "dart:io";
+
+List basicList() {
+  List simpleList = [1, 2, 3, 44];
+
+  simpleList.add(22);
+
+  simpleList.remove(3);
+
+  return (simpleList);
+}
+
+/**
+ * Lists are a basic data structure equivalent to arrays form C (I guess there will be some differences), 
+ * they work very simarly to arrays, so you can use indexes, and some higher order functions 
+ * like map, filter and reduce with them.
+ * 
+ * You declare Lists (I'll call them Arrays sometimes, lol) with the syntax:
+ * 
+ *  List varName = [//values separated by ',' (comma), don't forget the square brackets]
+ * 
+ * To add a new element we can use List.add(), and List.remove() to remove an element (by value)
+ * 
+ * We also have an indexOf function (to get a List of indexes of a value within a List)
+ * 
+ */
